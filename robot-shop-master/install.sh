e39156a6f944dcd36f2863b3922dc8294b830d73
Sriharsha Kalluru <skalluru@Sriharshas-MacBook-Pro.local>
2024-06-14 16:18:35 +0530

    Update

--- robot-shop-master/install.sh
+++ robot-shop-master/install.sh
@@ -13,6 +13,7 @@ systemctl start nginx
 cd $PWD
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 dnf install -y mongodb-org
-systemtl enable mongod
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf 
+systemctl enable mongod
 systemctl start mongod
 
