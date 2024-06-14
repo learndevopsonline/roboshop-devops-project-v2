052645e0e4cc72d9d9511fc27af3b8d1c0ea314f
Sriharsha Kalluru <skalluru@Sriharshas-MacBook-Pro.local>
2024-06-14 16:07:43 +0530

    Update

--- robot-shop-master/install.sh
+++ robot-shop-master/install.sh
@@ -3,7 +3,7 @@ dnf module enable nginx:1.24 -y
 
 dnf install nginx -y
 rm -rf /usr/share/nginx/html/*
-cp web/static/*  /usr/share/nginx/html/
+cp -r web/static/*  /usr/share/nginx/html/
 cp web/nginx.conf /etc/nginx/nginx.conf
 systemctl enable nginx
 systemctl start nginx
