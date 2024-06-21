7e5dd813694e25638e0c5394165bba24c9a7a37e
SVR Kalluru <raghuk.vit@gmail.com>
2024-06-21 15:42:23 +0530

    m

--- robot-shop-master/shipping/db/schema.sql
+++ robot-shop-master/shipping/db/schema.sql
@@ -5,6 +5,25 @@ create database if not exists cities;
 -- Replace 'localhost' with the desired host, if necessary
 use cities;
 
+DROP TABLE IF EXISTS `cities`;
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cities` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `country_code` varchar(2) DEFAULT NULL,
+  `city` varchar(100) DEFAULT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `region` varchar(100) DEFAULT NULL,
+  `latitude` decimal(10,7) DEFAULT NULL,
+  `longitude` decimal(10,7) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `region_idx` (`region`),
+  KEY `c_code_idx` (`country_code`),
+  FULLTEXT KEY `city_idx` (`city`)
+) ENGINE=InnoDB AUTO_INCREMENT=6223666 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
 DELIMITER $$
 
 -- Create a stored procedure to handle the user creation if not exists
@@ -34,21 +53,3 @@ CALL CreateUserIfNotExists();
 DROP PROCEDURE IF EXISTS CreateUserIfNotExists;
 
 
-DROP TABLE IF EXISTS `cities`;
-
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `cities` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `country_code` varchar(2) DEFAULT NULL,
-  `city` varchar(100) DEFAULT NULL,
-  `name` varchar(100) DEFAULT NULL,
-  `region` varchar(100) DEFAULT NULL,
-  `latitude` decimal(10,7) DEFAULT NULL,
-  `longitude` decimal(10,7) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `region_idx` (`region`),
-  KEY `c_code_idx` (`country_code`),
-  FULLTEXT KEY `city_idx` (`city`)
-) ENGINE=InnoDB AUTO_INCREMENT=6223666 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
